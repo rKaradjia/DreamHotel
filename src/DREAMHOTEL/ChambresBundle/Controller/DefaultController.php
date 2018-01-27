eaf3758be4753c00a4b06536bae3bb888e5a18b3
rKaradjia <rehane.karadjia.sio@gmail.com>
2018-01-27 16:25:18 +0100

    Samedi 27 Janvier 2018
    Les chambres : creation des pages et chainage
    Ajout de note quand a la futur BDD

--- src/DREAMHOTEL/ChambresBundle/Controller/DefaultController.php
+++ src/DREAMHOTEL/ChambresBundle/Controller/DefaultController.php
@@ -10,4 +10,21 @@ class DefaultController extends Controller
     {
         return $this->render('DREAMHOTELChambresBundle:Default:index.html.twig');
     }
+    
+    public function twinstandartAction()
+    {
+        return $this->render('DREAMHOTELChambresBundle:Twin:twinStandart.html.twig');
+    }
+    public function simplestandartAction()
+    {
+        return $this->render('DREAMHOTELChambresBundle:Simples:simpleStandart.html.twig');
+    }  
+    public function doublestandartAction()
+    {
+        return $this->render('DREAMHOTELChambresBundle:Doubles:doubleStandart.html.twig');
+    } 
+    public function suitedeluxeAction()
+    {
+        return $this->render('DREAMHOTELChambresBundle:Suites:suiteDseluxe.html.twig');
+    } 
 }
