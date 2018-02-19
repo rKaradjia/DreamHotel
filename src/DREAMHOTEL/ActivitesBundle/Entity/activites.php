4fa20dfecc907fe0608717a2cafbff1855395fc6
rKaradjia <rehane.karadjia.sio@gmail.com>
2018-02-19 12:41:47 +0100

    Lundi 19 Fevrier 2018
    Creation de la premiere entite

---
+++ src/DREAMHOTEL/ActivitesBundle/Entity/activites.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace DREAMHOTEL\ActivitesBundle\Entity;
+
+/**
+ * acceuil
+ */
+class activites
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $libelle;
+
+    /**
+     * @var string
+     */
+    private $prix;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set libelle
+     *
+     * @param string $libelle
+     *
+     * @return acceuil
+     */
+    public function setLibelle($libelle)
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * Get libelle
+     *
+     * @return string
+     */
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+
+    /**
+     * Set prix
+     *
+     * @param string $prix
+     *
+     * @return acceuil
+     */
+    public function setPrix($prix)
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    /**
+     * Get prix
+     *
+     * @return string
+     */
+    public function getPrix()
+    {
+        return $this->prix;
+    }
+}
+
